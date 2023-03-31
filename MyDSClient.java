f50a6be04fe490b6b7946332175d1ea3704d0085
46438688 <arion.dudley@students.mq.edu.au>
2023-03-31 14:14:58 +1100

    reorganized server catching and job scheduling to reliably catch and schedule given jobs

--- MyDSClient.java
+++ MyDSClient.java
@@ -21,7 +21,7 @@ public class MyDSClient {
 			String messageToSend = "HELO\n";
 			dataOut.write(messageToSend.getBytes());
 			String messageReceived = dataReader.readLine();
-			System.out.println(messageReceived);
+			System.out.println("Sent HELO and received " + messageReceived);
 			dataOut.flush();
 			
 			//get username and send AUTH
@@ -29,15 +29,33 @@ public class MyDSClient {
 			messageToSend = "AUTH" + username + "\n";
 			dataOut.write(messageToSend.getBytes());
 			messageReceived = dataReader.readLine();
-			System.out.println(messageReceived);
+			System.out.println("Confirmed username as " + username + " and received " + messageReceived);
 			dataOut.flush();
 			
 			//send REDY
 			messageToSend = "REDY\n";
 			dataOut.write(messageToSend.getBytes());
 			messageReceived = dataReader.readLine();
-			System.out.println(messageReceived);
+			System.out.println("Sent REDY and received " + messageReceived);
+			String[] jobData = messageReceived.split(" ");
+			String jobName = " ";
+			//print out job data for better readability of jobs
+			for(int i = 0; i < jobData.length; i++){
+				System.out.println("Job data at array location " + i + " is " + jobData[i]);
+			}
+			if(jobData[0].equals("JOBN") || jobData[0].equals("JCPL") || jobData[0].equals("NONE")){
+				jobName = jobData[0];
+				System.out.println("Job name set to " + jobName);
+			}
 			dataOut.flush();
+			
+			//while the last message from ds-server is not NONE do // jobs 1 - n
+			//if(!jobName.equals("NONE")){
+			//	int jobNum = jobData.length - 1;
+			//	while(jobNum != 0){
+			//		
+			//	}
+			//}
 			
 			//send GETS
 			messageToSend = "GETS All\n";
@@ -52,26 +70,26 @@ public class MyDSClient {
 			dataOut.flush();
 			
 			//process data
-			String[] data = messageReceived.split(" ");
-			int numServers = Integer.parseInt(data[1]);
+			String[] serverData = messageReceived.split(" ");
+			int numServers = Integer.parseInt(serverData[1]);
 			System.out.println(numServers);
 			
-			ArrayList<String[]> serverData = new ArrayList<String[]>();
+			ArrayList<String[]> serverDataArray = new ArrayList<String[]>();
 			
 			for(int i = 0; i < numServers; i++){
 				messageReceived = dataReader.readLine();
 				System.out.println(messageReceived);
-				//String[] serverData = messageReceived.split(" ");
-				serverData.add(messageReceived.split(" "));
+				//String[] serverDataArray = messageReceived.split(" ");
+				serverDataArray.add(messageReceived.split(" "));
 			}
 			
 			String largestServer = " ";
 			
-			for(int i = 0; i < serverData.size(); i++){
+			for(int i = 0; i < serverDataArray.size(); i++){
 				int numCPU = 0;
-				if(numCPU < Integer.parseInt(serverData.get(i)[4])) {
-					//numCPU = Integer.parseInt(serverData[4]);
-					largestServer = serverData.get(i)[0];
+				if(numCPU < Integer.parseInt(serverDataArray.get(i)[4])) {
+					//numCPU = Integer.parseInt(serverDataArray[4]);
+					largestServer = serverDataArray.get(i)[0];
 				}
 			}
 			
@@ -81,12 +99,31 @@ public class MyDSClient {
 			dataOut.flush();
 			
 			//schedule and send jobs
-			//int numCPU = Integer.parseInt(job[?]);
-			messageToSend = "SCHD " + "0 " + largestServer + " 0\n";
-			
+			//while the last message from ds-server is not NONE do // jobs 1 - n
+			if(jobName.equals("JOBN")){
+				
+				System.out.println("JOBN received, start scheduling job with job ID as " + jobData[2]);
+				messageToSend = "SCHD " + jobData[2] + " " + largestServer + " 0\n";
+				
+				//int jobNum = jobData.length - 1;
+				//int jobID = 1;
+				//System.out.println("Job name is set to allowed ID, start scheduling jobs with first job as " + jobData[jobID] + " and amount of jobs being " + jobNum);
+				//while(jobNum != 0){
+				//	messageToSend = "SCHD " + jobData[jobID] + " " + largestServer + " 0\n";
+				//	jobID++;
+				//	jobNum--;
+				//}
+			}
 			dataOut.write(messageToSend.getBytes());
 			messageReceived = dataReader.readLine();
 			System.out.println(messageReceived);
+			dataOut.flush();
+			
+			//send JCPL
+			messageToSend = "JCPL\n";
+			dataOut.write(messageToSend.getBytes());
+			messageReceived = dataReader.readLine();
+			System.out.println("Sent JCPL and received " + messageReceived);
 			dataOut.flush();
 			
 			//send QUIT after confirm job scheduled
@@ -95,20 +132,6 @@ public class MyDSClient {
 			sckt.close();
 			dataIn.close();
 			dataOut.close();
-			
-			//Socket sckt = new Socket("localhost",6666);
-			//DataOutputStream dataOut = new DataOutputStream(sckt.getOutputStream());
-			//dataOut.writeUTF("HELO");
-			//dataOut.flush();
-			//DataInputStream dis = new DataInputStream(sckt.getInputStream());
-			//String str = (String)dis.readUTF();
-			//System.out.println("Client received = " + str);
-			//dataOut.writeUTF("BYE");
-			//dataOut.flush();
-			//str = (String)dis.readUTF();
-			//System.out.println("Client received = " + str);
-			//dataOut.close();
-			//sckt.close();
 
 		} catch(Exception e) {
 			System.out.println(e);
